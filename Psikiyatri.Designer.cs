eedb440967630f41cb9e6462181d2f3cb9f0b4a8
busarslan <163575577+busarslan@users.noreply.github.com>
2024-06-12 21:50:41 +0300

    Add files via upload

---
+++ Psikiyatri.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Hastalik_Tani_Destek_Sistemi
+{
+    partial class Psikiyatri
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.chkDepression = new System.Windows.Forms.CheckBox();
+            this.chkAnxiety = new System.Windows.Forms.CheckBox();
+            this.chkPanicAttack = new System.Windows.Forms.CheckBox();
+            this.chkOCD = new System.Windows.Forms.CheckBox();
+            this.chkPTSD = new System.Windows.Forms.CheckBox();
+            this.chkSchizophrenia = new System.Windows.Forms.CheckBox();
+            this.chkBipolarDisorder = new System.Windows.Forms.CheckBox();
+            this.chkEatingDisorders = new System.Windows.Forms.CheckBox();
+            this.chkSleepDisorders = new System.Windows.Forms.CheckBox();
+            this.chkADHD = new System.Windows.Forms.CheckBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chkDepression
+            // 
+            this.chkDepression.AutoSize = true;
+            this.chkDepression.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkDepression.Location = new System.Drawing.Point(35, 28);
+            this.chkDepression.Name = "chkDepression";
+            this.chkDepression.Size = new System.Drawing.Size(96, 20);
+            this.chkDepression.TabIndex = 0;
+            this.chkDepression.Text = "Depresyon";
+            this.chkDepression.UseVisualStyleBackColor = true;
+            // 
+            // chkAnxiety
+            // 
+            this.chkAnxiety.AutoSize = true;
+            this.chkAnxiety.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkAnxiety.Location = new System.Drawing.Point(35, 55);
+            this.chkAnxiety.Name = "chkAnxiety";
+            this.chkAnxiety.Size = new System.Drawing.Size(88, 20);
+            this.chkAnxiety.TabIndex = 1;
+            this.chkAnxiety.Text = "Anksiyete";
+            this.chkAnxiety.UseVisualStyleBackColor = true;
+            // 
+            // chkPanicAttack
+            // 
+            this.chkPanicAttack.AutoSize = true;
+            this.chkPanicAttack.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkPanicAttack.Location = new System.Drawing.Point(35, 82);
+            this.chkPanicAttack.Name = "chkPanicAttack";
+            this.chkPanicAttack.Size = new System.Drawing.Size(93, 20);
+            this.chkPanicAttack.TabIndex = 2;
+            this.chkPanicAttack.Text = "Panik Atak";
+            this.chkPanicAttack.UseVisualStyleBackColor = true;
+            // 
+            // chkOCD
+            // 
+            this.chkOCD.AutoSize = true;
+            this.chkOCD.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkOCD.Location = new System.Drawing.Point(35, 109);
+            this.chkOCD.Name = "chkOCD";
+            this.chkOCD.Size = new System.Drawing.Size(58, 20);
+            this.chkOCD.TabIndex = 3;
+            this.chkOCD.Text = "OCD";
+            this.chkOCD.UseVisualStyleBackColor = true;
+            // 
+            // chkPTSD
+            // 
+            this.chkPTSD.AutoSize = true;
+            this.chkPTSD.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkPTSD.Location = new System.Drawing.Point(35, 136);
+            this.chkPTSD.Name = "chkPTSD";
+            this.chkPTSD.Size = new System.Drawing.Size(66, 20);
+            this.chkPTSD.TabIndex = 4;
+            this.chkPTSD.Text = "PTSD";
+            this.chkPTSD.UseVisualStyleBackColor = true;
+            // 
+            // chkSchizophrenia
+            // 
+            this.chkSchizophrenia.AutoSize = true;
+            this.chkSchizophrenia.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkSchizophrenia.Location = new System.Drawing.Point(35, 163);
+            this.chkSchizophrenia.Name = "chkSchizophrenia";
+            this.chkSchizophrenia.Size = new System.Drawing.Size(80, 20);
+            this.chkSchizophrenia.TabIndex = 5;
+            this.chkSchizophrenia.Text = "Şizofreni";
+            this.chkSchizophrenia.UseVisualStyleBackColor = true;
+            // 
+            // chkBipolarDisorder
+            // 
+            this.chkBipolarDisorder.AutoSize = true;
+            this.chkBipolarDisorder.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkBipolarDisorder.Location = new System.Drawing.Point(35, 190);
+            this.chkBipolarDisorder.Name = "chkBipolarDisorder";
+            this.chkBipolarDisorder.Size = new System.Drawing.Size(129, 20);
+            this.chkBipolarDisorder.TabIndex = 6;
+            this.chkBipolarDisorder.Text = "Bipolar Bozukluk";
+            this.chkBipolarDisorder.UseVisualStyleBackColor = true;
+            // 
+            // chkEatingDisorders
+            // 
+            this.chkEatingDisorders.AutoSize = true;
+            this.chkEatingDisorders.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkEatingDisorders.Location = new System.Drawing.Point(35, 217);
+            this.chkEatingDisorders.Name = "chkEatingDisorders";
+            this.chkEatingDisorders.Size = new System.Drawing.Size(140, 20);
+            this.chkEatingDisorders.TabIndex = 7;
+            this.chkEatingDisorders.Text = "Yeme Bozuklukları";
+            this.chkEatingDisorders.UseVisualStyleBackColor = true;
+            // 
+            // chkSleepDisorders
+            // 
+            this.chkSleepDisorders.AutoSize = true;
+            this.chkSleepDisorders.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkSleepDisorders.Location = new System.Drawing.Point(35, 244);
+            this.chkSleepDisorders.Name = "chkSleepDisorders";
+            this.chkSleepDisorders.Size = new System.Drawing.Size(135, 20);
+            this.chkSleepDisorders.TabIndex = 8;
+            this.chkSleepDisorders.Text = "Uyku Bozuklukları";
+            this.chkSleepDisorders.UseVisualStyleBackColor = true;
+            // 
+            // chkADHD
+            // 
+            this.chkADHD.AutoSize = true;
+            this.chkADHD.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.chkADHD.Location = new System.Drawing.Point(35, 271);
+            this.chkADHD.Name = "chkADHD";
+            this.chkADHD.Size = new System.Drawing.Size(238, 20);
+            this.chkADHD.TabIndex = 9;
+            this.chkADHD.Text = "Dikkat Eksikliği ve Hiperaktivite DB";
+            this.chkADHD.UseVisualStyleBackColor = true;
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            this.lblResult.Location = new System.Drawing.Point(35, 324);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 16);
+            this.lblResult.TabIndex = 11;
+            // 
+            // Psikiyatri
+            // 
+            this.BackColor = Color.Snow;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.ClientSize = new System.Drawing.Size(654, 387);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.chkADHD);
+            this.Controls.Add(this.chkSleepDisorders);
+            this.Controls.Add(this.chkEatingDisorders);
+            this.Controls.Add(this.chkBipolarDisorder);
+            this.Controls.Add(this.chkSchizophrenia);
+            this.Controls.Add(this.chkPTSD);
+            this.Controls.Add(this.chkOCD);
+            this.Controls.Add(this.chkPanicAttack);
+            this.Controls.Add(this.chkAnxiety);
+            this.Controls.Add(this.chkDepression);
+            this.Name = "Psikiyatri";
+            this.Text = "Psikiyatri Belirti Kontrolü";
+
+            // Arka plan resmini ekleyin
+            this.BackgroundImage = Hastalik_Tani_Destek_Sistemi.Properties.Resources.GeneralBackground;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.CheckBox chkDepression;
+        private System.Windows.Forms.CheckBox chkAnxiety;
+        private System.Windows.Forms.CheckBox chkPanicAttack;
+        private System.Windows.Forms.CheckBox chkOCD;
+        private System.Windows.Forms.CheckBox chkPTSD;
+        private System.Windows.Forms.CheckBox chkSchizophrenia;
+        private System.Windows.Forms.CheckBox chkBipolarDisorder;
+        private System.Windows.Forms.CheckBox chkEatingDisorders;
+        private System.Windows.Forms.CheckBox chkSleepDisorders;
+        private System.Windows.Forms.CheckBox chkADHD;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
